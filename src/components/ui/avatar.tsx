9b12178486be0e31cde986dde3d9ce70da8d37b4
Advant0-st4r <advantoincorporated@gmail.com>
2025-09-27 17:47:37 +0300

    Update avatar.tsx

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -45,3 +45,5 @@ const AvatarFallback = React.forwardRef<
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
 
 export { Avatar, AvatarImage, AvatarFallback };
+
+export { Avatar, AvatarImage, AvatarFallback };
